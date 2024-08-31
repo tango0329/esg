9d2ad5f9f361f42c7c68590c6eb84e85d5da7a6a
ciel <165789365+tango0329@users.noreply.github.com>
2024-08-31 17:35:37 +0900

    init
    init

---
+++ src/main/java/com/example/demo/EsgApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgApplication.class, args);
+		System.out.println("-------------------");
+	}
+
+
+}
